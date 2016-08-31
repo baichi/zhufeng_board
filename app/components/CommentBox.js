d803cc8b066a727fac7efb3e2ca4d4305186afee
zhufengnodejs <zhufengnodejs@126.com>
2016-08-31 09:37:57 +0800

    6.从远程接口获取数据并改变状态

--- app/components/CommentBox.js
+++ app/components/CommentBox.js
@@ -1,10 +1,30 @@
 'use strict'
-
+import $ from 'jquery';
 import React from 'react';
 import CommentList from './CommentList.js';
 import CommentForm from './CommentForm.js';
 import 'bootstrap/dist/css/bootstrap.css';
 class CommentBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {comments: []};
+        this.getComments();
+    }
+
+    getComments() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: data => {
+                this.setState({comments: data});
+            },
+            error: (xhr, status, error)=> {
+                console.error(error);
+            }
+        })
+    }
+
     render() {
         return (
             <div className="row">
@@ -12,7 +32,7 @@ class CommentBox extends React.Component {
                     <div class="page-header">
                         <h1>评论</h1>
                     </div>
-                    <CommentList data={this.props.data}/>
+                    <CommentList data={this.state.comments}/>
                 </div>
                 <div className="col-xs-12">
                     <CommentForm/>
