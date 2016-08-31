b99786c39343a11325a8adfd1a73a4fd923e838e
zhufengnodejs <zhufengnodejs@126.com>
2016-08-31 09:23:25 +0800

    2.创建第一个组件CommentBox

---
+++ app/components/CommentBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-xs-12">
+                    <div class="page-header">
+                        <h1>评论</h1>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export {CommentBox as default}
---
+++ app/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBox from './components/CommentBox.js';
+
+
+ReactDOM.render(<CommentBox />, document.querySelector('#app'));
